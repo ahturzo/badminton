7f835983e4a6828e3397a9618138fd9382524d81
ahturzo <tabidhasan003@gmail.com>
2019-12-28 17:07:16 +0600

    qualify5

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -106,7 +106,7 @@
                             @php $i =0; @endphp
                             @foreach($point as $team)
                                 @if($i == 0 || $i == 1 || $i == 2 || $i == 3)
-                                <tr class="text-white" style="background-color: black;">
+                                <tr class="text-white" style="background-color: #006400;">
                                 @else
                                 <tr class="table-success">
                                 @endif
